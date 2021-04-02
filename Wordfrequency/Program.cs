f9370de374469f0a99426ce1c0f9faf0aa4a94c6
Domadanar <denik8223@gmail.com>
2021-04-02 07:32:17 +0300

    Добавьте файлы проекта.

---
+++ Wordfrequency/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Wordfrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = @"Peter Piper picked a peck peck of pickled peppers peppers.";
+            var matches = Regex.Split(data.ToLower(), "\r\n");
+
+            Dictionary<string, Tmp> dct = new Dictionary<string, Tmp>();
+            int index = 1;
+            foreach (var sent in matches)
+            {
+                var words = Regex.Matches(sent, @"\w+(-\w+)*").Cast<Match>().Select(i => i.Value).ToArray();
+                foreach (var word in words)
+                {
+                    if (!dct.ContainsKey(word))
+                        dct.Add(word, new Tmp() { Rows = new HashSet<int>() });
+
+                    var value = dct[word];
+                    value.Rows.Add(index);
+                    value.Count++;
+                }
+                index++;
+            }
+
+            foreach (var item in dct.OrderBy(b => b.Key))
+            {
+                Console.WriteLine("{0}{1}", item.Key.PadRight(10, '_'), item.Value.Count);
+            }
+            Console.ReadKey();
+        }
+        class Tmp
+        {
+            public int Count;
+            public HashSet<int> Rows;
+        }
+    }
+}
